bf3897c17e2ad4c110706f01444d6b4629e98414
benkhtai <benkhtai@nbwin1249.campus.tue.nl>
2016-12-13 11:14:05 +0100

    updated Pattern class and Glider pattern

---
+++ src/gol/ExploderPattern.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gol;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author benkhtai
+ */
+public class ExploderPattern implements Pattern {
+    
+     public int height = 0;
+    public int width = 0;
+    public List<CellState> patternList = new ArrayList();
+
+    
+    public ExploderPattern()
+    {
+    
+        height = 4;
+        width = 3;
+        patternList.add(CellState.dead);
+        patternList.add(CellState.alive);
+        patternList.add(CellState.dead);
+        patternList.add(CellState.alive);
+        patternList.add(CellState.alive);
+        patternList.add(CellState.alive);
+        patternList.add(CellState.alive);
+        patternList.add(CellState.dead);
+        patternList.add(CellState.alive);
+        patternList.add(CellState.dead);
+        patternList.add(CellState.alive);
+        patternList.add(CellState.dead);
+        
+   }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getWidth() {
+       return width;
+    }
+
+    @Override
+    public List<CellState> getPatternArray() {
+        return patternList;
+    }
+    
+}
